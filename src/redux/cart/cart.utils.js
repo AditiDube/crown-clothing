197924a3dc263aa9b8bb021aaadbdc37f8824332
Aditi Dubey <adubey26@in.ibm.com>
2020-06-28 16:09:50 +0530

    add quantity and reduce/remove quantity functionality added to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+export const removeitemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+
+    if (existingCartItem) {
+        if (existingCartItem.quantity == 1) {
+            return cartItems.filter(
+                cartItem => cartItem.id !== cartItemToRemove.id
+            )
+        } else {
+            return cartItems.map(
+                cartItem => cartItem.id === cartItemToRemove.id
+                    ? { ...cartItem, quantity: cartItem.quantity - 1 }
+                    : cartItem
+            )
+        }
+    }
+}
